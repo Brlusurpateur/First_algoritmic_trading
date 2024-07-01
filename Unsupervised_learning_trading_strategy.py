8eb3d24aafc6c7360b0906406693893d22abec86
Brlusurpateur <moncoucut.brandon@gmail.com>
2024-07-01 12:48:39 +0200

    Modif 2024-07-01

--- Unsupervised_learning_trading_strategy.py
+++ Unsupervised_learning_trading_strategy.py
@@ -38,8 +38,8 @@ symbols_list = sp500['Symbol'].unique().tolist()
 #Téléchargement des prix journalier de l'ensemble des indices du S&P500 d'il y a 8 ans à hier.
 today = dt.date.today()
 yesterday = today - dt.timedelta(days=1)
-#end_date = yesterday.strftime('%Y-%m-%d')
-end_date = '2023-09-27'
+end_date = yesterday.strftime('%Y-%m-%d')
+#end_date = '2023-09-27'
 start_date = pd.to_datetime(end_date)-pd.DateOffset(365*8)
 df = pd.DataFrame(yf.download(tickers=symbols_list, start=start_date, end=end_date))
 #Stack est utilisé pour empiler les niveau de colonnes d'un DataFrame.
@@ -289,17 +289,26 @@ from pypfopt import expected_returns
 
 def optimize_weights(prices, lower_bound=0):
 
+    #Calculate average historical yields
     returns = expected_returns.mean_historical_return(prices=prices,
                                                       frequency=252)
     
+    #Calculate the covariance matrix of returns
     cov = risk_models.sample_cov(prices=prices,
                                  frequency=252)
     
+    #Ensure that the covariance matrix is symmetrical
+    cov = (cov + cov.T) / 2
+    #print("Dimensions des prix:", prices.shape)
+    #print(cov)
+
+    #Initialise the EfficientFrontier object
     ef = EfficientFrontier(expected_returns=returns,
                            cov_matrix=cov,
                            weight_bounds=(lower_bound, .1),
                            solver='SCS')
     
+    #Optimising weights to maximise the sharpe ratio
     weights = ef.max_sharpe()
 
     return ef.clean_weights()
@@ -330,12 +339,29 @@ for start_date in fixed_dates.keys():
     optimization_start_date = (pd.to_datetime(start_date)-pd.DateOffset(months=12)).strftime('%Y-%m-%d')
     optimization_end_date = (pd.to_datetime(start_date)-pd.DateOffset(days=1)).strftime('%Y-%m-%d')
 
-#optimize_df = new_df[optimization_start_date:optimization_end_date]['Adj Close'][cols]
-optimization_df = new_df['Adj Close']['2016-11-01':'2017-10-30'][cols]
+    optimization_df = new_df['Adj Close'][optimization_start_date:optimization_end_date][cols]
+    #Find 0.01
+    #weights = optimize_weights(prices=optimization_df,
+                           #lower_bound=round(1/(len(optimization_df.columns)*2),2))
+    # Find 0.012
+    weights = optimize_weights(prices=optimization_df,
+                           lower_bound=round(1/(len(optimization_df.columns)*2),3))
+    #print(round(1/(len(optimization_df.columns)*2),3))
+    weights = pd.DataFrame(weights, index=pd.Series(0))
 
-weights = optimize_weights(prices=optimization_df,
-                           lower_bound=0)
+    temp_df = returns_dataframe[start_date:end_date]
 
-print(len(optimization_df.columns))
+    temp_df = temp_df.stack().to_frame('return').reset_index(level=0)\
+       .merge(weights.stack().to_frame('weight').reset_index(level=0, drop=True),
+              left_index=True,
+              right_index=True)\
+       .reset_index().set_index(['Date', 'index']).unstack().stack()
+    
+    temp_df.index.names = ['date', 'ticker']
+    temp_df['weighted_return'] = temp_df['return']*temp_df['weight']
+    temp_df = temp_df.groupby(level=0)['weighted_return'].sum().to_frame('Strategy Return')
+    portfolio_df = pd.concat([portfolio_df, temp_df], axis=0)
+    print(temp_df)
 
-## !!!!!!!!! problem with the function optimize_weights
+# Print the fixed_dates keys to check which dates are actually present
+#print("Clés de fixed_dates:", list(fixed_dates.keys()))
