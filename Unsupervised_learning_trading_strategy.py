2bd4e7e6d5a2ed331a538292aebec0707170be23
Brlusurpateur <moncoucut.brandon@gmail.com>
2024-07-02 19:25:19 +0200

    Modif 2024-07-02

--- Unsupervised_learning_trading_strategy.py
+++ Unsupervised_learning_trading_strategy.py
@@ -333,35 +333,56 @@ returns_dataframe = np.log(new_df['Adj Close']).diff()
 portfolio_df = pd.DataFrame()
 
 for start_date in fixed_dates.keys():
+        
+    try:
     
-    end_date = (pd.to_datetime(start_date)+pd.offsets.MonthEnd(0)).strftime('%Y-%m-%d')
-    cols = fixed_dates[start_date]
-    optimization_start_date = (pd.to_datetime(start_date)-pd.DateOffset(months=12)).strftime('%Y-%m-%d')
-    optimization_end_date = (pd.to_datetime(start_date)-pd.DateOffset(days=1)).strftime('%Y-%m-%d')
+        end_date = (pd.to_datetime(start_date)+pd.offsets.MonthEnd(0)).strftime('%Y-%m-%d')
+        cols = fixed_dates[start_date]
+        optimization_start_date = (pd.to_datetime(start_date)-pd.DateOffset(months=12)).strftime('%Y-%m-%d')
+        optimization_end_date = (pd.to_datetime(start_date)-pd.DateOffset(days=1)).strftime('%Y-%m-%d')
 
-    optimization_df = new_df['Adj Close'][optimization_start_date:optimization_end_date][cols]
-    #Find 0.01
-    #weights = optimize_weights(prices=optimization_df,
-                           #lower_bound=round(1/(len(optimization_df.columns)*2),2))
-    # Find 0.012
-    weights = optimize_weights(prices=optimization_df,
-                           lower_bound=round(1/(len(optimization_df.columns)*2),3))
-    #print(round(1/(len(optimization_df.columns)*2),3))
-    weights = pd.DataFrame(weights, index=pd.Series(0))
+        optimization_df = new_df['Adj Close'][optimization_start_date:optimization_end_date][cols]
 
-    temp_df = returns_dataframe[start_date:end_date]
+        success = False
+        try:
+            #Find 0.01
+            #weights = optimize_weights(prices=optimization_df,
+                                #lower_bound=round(1/(len(optimization_df.columns)*2),2))
+            # Find 0.012
+            weights = optimize_weights(prices=optimization_df,
+                                lower_bound=round(1/(len(optimization_df.columns)*2),3))
+            #print(round(1/(len(optimization_df.columns)*2),3))
+            weights = pd.DataFrame(weights, index=pd.Series(0))
 
-    temp_df = temp_df.stack().to_frame('return').reset_index(level=0)\
-       .merge(weights.stack().to_frame('weight').reset_index(level=0, drop=True),
-              left_index=True,
-              right_index=True)\
-       .reset_index().set_index(['Date', 'index']).unstack().stack()
+            success = True
+        except:
+            print(f'Max Sharpe Optimization failed for {start_date}, Continuing with Equal-Weights')
+
+        if success==False:
+            weights = pd.DataFrame([1/len(optimization_df.columns) for i in range(len(optimization_df.columns))],
+                                   index=optimization_df.columns.tolist(),
+                                   columns=pd.Series(0)).T
+
+        temp_df = pd.DataFrame(returns_dataframe[start_date:end_date])
+
+        # Impression pour vérifier les colonnes avant la fusion
+        print("Colonnes de temp_df avant la fusion:", temp_df.columns)
+        print("Colonnes de weights avant la fusion:", weights.columns)
+
+        temp_df = temp_df.stack().to_frame('return').reset_index(level=0)\
+                    .merge(weights.stack().to_frame('weight').reset_index(level=0, drop=True),
+                            left_index=True,
+                            right_index=True)\
+                    .reset_index().set_index(['Date', 'index']).unstack().stack()
+        
+        temp_df.index.names = ['date', 'ticker']
+        temp_df['weighted_return'] = temp_df['return']*temp_df['weight']
+        temp_df = temp_df.groupby(level=0)['weighted_return'].sum().to_frame('Strategy Return')
+        portfolio_df = pd.concat([portfolio_df, temp_df], axis=0)
     
-    temp_df.index.names = ['date', 'ticker']
-    temp_df['weighted_return'] = temp_df['return']*temp_df['weight']
-    temp_df = temp_df.groupby(level=0)['weighted_return'].sum().to_frame('Strategy Return')
-    portfolio_df = pd.concat([portfolio_df, temp_df], axis=0)
-    print(temp_df)
+    except Exception as e:
+        print(e)
 
+portfolio_df.plot()
 # Print the fixed_dates keys to check which dates are actually present
 #print("Clés de fixed_dates:", list(fixed_dates.keys()))
